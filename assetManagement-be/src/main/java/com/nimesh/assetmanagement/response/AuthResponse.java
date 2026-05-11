2e6e25a19f9e9fa12e2b346fb2b174ff17e31e64
Nimesh Jayaweera <nim3xh@gmail.com>
2026-05-11 11:57:11 +0530

    add APIResponse and AuthResponse classes

---
+++ assetManagement-be/src/main/java/com/nimesh/assetmanagement/response/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.nimesh.assetmanagement.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nimesh.assetmanagement.entity.User;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthResponse {
+    private int statusCode;
+    private String error;
+    private String message;
+    private String data;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String name;
+    private String email;
+    private String role;
+    private String password;
+    private User user;
+}
