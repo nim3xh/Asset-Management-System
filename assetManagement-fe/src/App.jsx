8d498c6d27e063a4a2268e922d91c7a7abaad56e
Nimesh Jayaweera <nim3xh@gmail.com>
2026-05-11 15:25:47 +0530

    feat: implement application routing with authenticated layout and sidebar navigation component

--- assetManagement-fe/src/App.jsx
+++ assetManagement-fe/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from '../features/auth/AuthContext'
 import { PrivateRoute } from './components/PrivateRoute'
 import { AuthPage } from './pages/AuthPage'
-import { Dashboard } from './pages/Dashboard'
 import { BrandManager } from './pages/BrandManager'
 import { DeviceManager } from './pages/DeviceManager'
 import { UserManager } from './pages/UserManager'
@@ -18,15 +17,14 @@ export default function App() {
           <Route path="/login" element={<AuthPage />} />
           
           <Route element={<PrivateRoute><Layout /></PrivateRoute>}>
-            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/devices" element={<DeviceManager />} />
             <Route path="/brands" element={<BrandManager />} />
             <Route path="/users" element={<UserManager />} />
             <Route path="/assignments" element={<AssignmentManager />} />
           </Route>
 
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/" element={<Navigate to="/devices" replace />} />
+          <Route path="*" element={<Navigate to="/devices" replace />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
